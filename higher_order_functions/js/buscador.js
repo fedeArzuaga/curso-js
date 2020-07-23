bb3bebeca0e35c8c02be341c876173b15f50d175
fedeArzuaga <fede.arzuaga.perdomo@gmail.com>
2020-07-23 08:57:24 -0300

    Adding the lasts files of High Order Functions and learning to use IndexedDB

--- higher_order_functions/js/buscador.js
+++ higher_order_functions/js/buscador.js
@@ -9,3 +9,187 @@ for(let i = max; i >  min; i--) {
     option.innerText = i;
     document.querySelector('#year').appendChild(option);
 }
+
+function obtenerAutos() {
+    return [
+        {
+            marca: 'BMW',
+            modelo: 'Serie 3',
+            year: 2012,
+            precio: 30000,
+            puertas: 4,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        { marca: 'Audi', modelo: 'A4', year: 2018, precio: 40000, puertas: 4, color: 'Negro', transmision: 'automatico' },
+        {
+            marca: 'Ford',
+            modelo: 'Mustang',
+            year: 2015,
+            precio: 20000,
+            puertas: 2,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        { marca: 'Audi', modelo: 'A6', year: 2010, precio: 35000, puertas: 4, color: 'Negro', transmision: 'automatico' },
+        {
+            marca: 'BMW',
+            modelo: 'Serie 5',
+            year: 2016,
+            precio: 70000,
+            puertas: 4,
+            color: 'Rojo',
+            transmision: 'automatico'
+        },
+        {
+            marca: 'Mercedes Benz',
+            modelo: 'Clase C',
+            year: 2015,
+            precio: 25000,
+            puertas: 4,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        {
+            marca: 'Chevrolet',
+            modelo: 'Camaro',
+            year: 2018,
+            precio: 60000,
+            puertas: 2,
+            color: 'Rojo',
+            transmision: 'manual'
+        },
+        { marca: 'Ford', modelo: 'Mustang', year: 2019, precio: 80000, puertas: 2, color: 'Rojo', transmision: 'manual' },
+        {
+            marca: 'Dodge',
+            modelo: 'Challenger',
+            year: 2017,
+            precio: 40000,
+            puertas: 4,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        { marca: 'Audi', modelo: 'A3', year: 2017, precio: 55000, puertas: 2, color: 'Negro', transmision: 'manual' },
+        {
+            marca: 'Dodge',
+            modelo: 'Challenger',
+            year: 2012,
+            precio: 25000,
+            puertas: 2,
+            color: 'Rojo',
+            transmision: 'manual'
+        },
+        {
+            marca: 'Mercedes Benz',
+            modelo: 'Clase C',
+            year: 2018,
+            precio: 45000,
+            puertas: 4,
+            color: 'Azul',
+            transmision: 'automatico'
+        },
+        {
+            marca: 'BMW',
+            modelo: 'Serie 5',
+            year: 2019,
+            precio: 90000,
+            puertas: 4,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        { marca: 'Ford', modelo: 'Mustang', year: 2017, precio: 60000, puertas: 2, color: 'Negro', transmision: 'manual' },
+        {
+            marca: 'Dodge',
+            modelo: 'Challenger',
+            year: 2015,
+            precio: 35000,
+            puertas: 2,
+            color: 'Azul',
+            transmision: 'automatico'
+        },
+        {
+            marca: 'BMW',
+            modelo: 'Serie 3',
+            year: 2018,
+            precio: 50000,
+            puertas: 4,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        {
+            marca: 'BMW',
+            modelo: 'Serie 5',
+            year: 2017,
+            precio: 80000,
+            puertas: 4,
+            color: 'Negro',
+            transmision: 'automatico'
+        },
+        {
+            marca: 'Mercedes Benz',
+            modelo: 'Clase C',
+            year: 2018,
+            precio: 40000,
+            puertas: 4,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        { marca: 'Audi', modelo: 'A4', year: 2016, precio: 30000, puertas: 4, color: 'Azul', transmision: 'automatico' }
+    ];
+}
+
+let datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transimision: '',
+    color: ''
+}
+
+const autos = obtenerAutos();
+
+document.addEventListener( "DOMContentLoaded", () => {
+
+    mostrarAutos(autos);
+
+})
+
+function mostrarAutos( arr ) {
+
+    const content = document.querySelector("#resultado");
+    
+    arr.forEach( item => {
+
+        const autoHTML = document.createElement("p");
+        autoHTML.innerHTML = `<p>Marca: ${item.marca} - Modelo: ${item.modelo} - Año: ${item.year} - N puertas: ${item.puertas} - Transimision: ${item.transmision} - Precio: ${item.precio} - Color: ${item.color}</p>`;
+        content.appendChild(autoHTML);
+
+    })
+
+}
+
+function filtrarAuto() {
+
+    const resultado = obtenerAutos().filter(filtrarMarca);
+    console.log(resultado);
+
+}
+
+function filtrarMarca(auto) {
+    if( datosBusqueda.marca ) {
+        return auto.marca === datosBusqueda.marca;
+    }
+}
+
+// Formulario
+
+const form = document.querySelector("#buscador")
+
+form.marca.addEventListener("input", e => {
+    datosBusqueda.marca = e.target.value;
+    filtrarAuto();
+});
+
+
---
+++ indexedDB/js/app.js
@@ -0,0 +1,133 @@
+let DB;
+
+// Selectores de la interfaz
+const form = document.querySelector('form');
+const citas = document.querySelector('#citas');
+const heading = document.querySelector('#administra');
+
+// Esperar por el DOM Ready
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Crear la base de datos
+    let crearDB = window.indexedDB.open('citas', 1);
+
+    // Si hay un error, a la consola
+    crearDB.onerror = function() {
+        console.log("Hubo un error");
+    }
+
+    // Si todo esta bien, muestra en consola y asignar la base de datos
+    crearDB.onsuccess = function() {
+        //console.log('todo listo');
+
+        // Asignar a la base de datos
+
+        DB = crearDB.result;
+        //console.log(DB);
+
+        mostrarCitas();
+
+    }
+
+    // Este metodo solo correo una vez y es ideal para crear el schema
+    crearDB.onupgradeneeded = function(e) {
+
+        // El evento es la misma base de datos
+        let db = e.target.result;
+
+        // definir el objecstore, toma 2 parametos: el nombre de la base de datos y las opciones
+        let objecStore = db.createObjectStore("citas", {
+            keyPath: 'key', // Es el indice de la base de datos
+            autoIncrement: true // Autoincrementa por registro
+        });
+
+        objecStore.createIndex('mascota', 'mascota', {
+            unique: false 
+        });
+
+        objecStore.createIndex('cliente', 'cliente', {
+            unique: false 
+        });
+
+        objecStore.createIndex('telefono', 'telefono', {
+            unique: false 
+        });
+
+        objecStore.createIndex('fecha', 'fecha', {
+            unique: false 
+        });
+
+        objecStore.createIndex('hora', 'hora', {
+            unique: false 
+        });
+
+        objecStore.createIndex('sintmoas', 'mascota', {
+            unique: false 
+        });
+        
+    }
+
+    // Cuando el form se envia
+    form.addEventListener('submit', agregarDatos)
+
+    function agregarDatos(e) {
+        
+        e.preventDefault();
+
+        const data = {
+            mascota: form.mascota.value,
+            cliente: form.cliente.value,
+            telefono: form.telefono.value,
+            fecha: form.fecha.value,
+            hora: form.hora.value,
+            sintomas: form.sintomas.value
+        }
+
+        // console.log(data);
+
+        // en IndexDB se utilizan las transacciones
+
+        let transaction = DB.transaction(['citas'], 'readwrite');
+        let objectStore = transaction.objectStore('citas');
+        let peticion = objectStore.add(data);
+
+        console.log(peticion);
+
+        peticion.onsuccess = () => {
+            form.reset();
+        }
+
+        transaction.oncomplete = () => {
+            console.log('Cita agregada');
+        }
+
+        transaction.onerror = () => {
+            console.log('Hubo un error');
+        }
+
+    }
+
+    function mostrarCitas() {
+
+        // limpiar las citas anteriores
+        while( citas.firstChild ) {
+            citas.removeChild(citas.firstChild);
+        }
+
+        // Creamos un objectStore
+
+        let objectStore = DB.transaction('citas').objectStore('citas');
+
+        // Esto retorna una peticion
+        objectStore.openCursor().onsuccess = function(e) {
+
+            // Cursos se va a ubicar en el registro ubicado
+            let cursor = e.target.cursor;
+
+            console.log( cursor );
+
+        }
+
+    }
+
+});
