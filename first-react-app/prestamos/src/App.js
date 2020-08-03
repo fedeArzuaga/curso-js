3eda5d9074b17cd288ca4ffc3c92f88ee9bb5ecc
fedeArzuaga <fede.arzuaga.perdomo@gmail.com>
2020-08-03 06:21:09 -0300

    Modifying these react files

--- first-react-app/prestamos/src/App.js
+++ first-react-app/prestamos/src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+
+      {/* My first time using React!!! YAAAYYY! */}
+
       <h1>Hello World!</h1>
     </div>
   );
